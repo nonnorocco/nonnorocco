fa381bdf9c7d62a5420a57d8c4506ae4d8b7f6dd
nonnorocco <pasqualesorace10@gmail.com>
2026-08-01 09:18:54 +0000

    Remove cloudflare deployment files

--- worker.js
+++
@@ -1,8 +0,0 @@
-export default {
-  async fetch(request) {
-    return new Response(
-      "Worker: " + new URL(request.url).pathname,
-      { headers: { "Content-Type": "text/plain" } }
-    );
-  }
-};
